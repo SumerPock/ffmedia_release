eb836a11596b5cc6b24697cf0d318b2b51f62a41
Eushally <miui2312801459@gmail.com>
2024-08-19 18:00:24 +0800

    增加一些注释

--- demo/osd.cpp
+++ demo/osd.cpp
@@ -1,5 +1,4 @@
 #include "osd.hpp"
-
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -34,16 +33,22 @@ static void parseORotate(std::stringstream& lineStream, ModuleOsd::OsdConfPara& 
     conf.oRotate = RgaRotate(r);
 }
 
+/// @brief 这段代码的主要目的是从 std::stringstream 中解析出一个字符串，并处理可能包含的引号。如果字符串是被双引号包围的，那么它会去除引号并提取其中的内容
+/// @param lineStream
+/// @param str
 static void parseTextString(std::stringstream& lineStream, std::string& str)
 {
-    lineStream >> str;
-    if (str[0] == '"') {
+    lineStream >> str; // 从 lineStream 中提取一个单词并存储到 str 中。这个单词是按空白字符（空格、制表符等）分隔的
+    if (str[0] == '"') //双引号检查
+    {
         std::string::size_type pos1 = 0;
         std::string::size_type pos2 = 0;
         std::string line = lineStream.str();
-        if ((pos1 = line.find('\"')) != std::string::npos) {
+        if ((pos1 = line.find('\"')) != std::string::npos) 
+        {
             pos2 = line.rfind('\"');
-            if (pos2 != std::string::npos && pos2 > pos1) {
+            if (pos2 != std::string::npos && pos2 > pos1) 
+            {
                 str.assign(line, pos1 + 1, pos2 - pos1 - 1);
             }
         }
@@ -238,60 +243,91 @@ ModuleOsd::~ModuleOsd()
     stop();
 }
 
+/// @brief 这段代码的目的是解析一个配置文件，将其中的配置信息加载到 OsdConfPara 对象中。
+///        它逐行读取配置文件，跳过注释和空行，并根据每一行的关键字调用相应的解析函数来处理具体的配置项
+/// @param conf
+/// @return
 int ModuleOsd::parseConfFileInfo(OsdConfPara& conf)
 {
-    if (conf_file.empty())
-        return -1;
+    if (conf_file.empty())  //配置文件路径是否为空
+        return -1;          
 
-    char* fileData = readFile(conf_file.c_str());
+    //读取配置文件内容
+    char *fileData = readFile(conf_file.c_str()); // 使用 readFile 函数读取配置文件内容，并将其存储在 fileData 指针中
     if (!fileData)
         return -1;
 
-    std::stringstream strStream(fileData);
+    //创建字符串流
+    std::stringstream strStream(fileData); // 将文件内容（fileData）加载到 std::stringstream 对象 strStream 中，用于逐行处理配置文件
     delete fileData;
 
     std::string temp;
     const int MAX_SIZE = 512;
     char lineStr[MAX_SIZE];
 
+    // 使用 getline 方法从 strStream 中读取一行数据到 lineStr 中，直到文件末尾（eof）为止。
     while (!strStream.eof()) {
         strStream.getline(lineStr, MAX_SIZE);
 
+        //跳过注释和空行
         if (lineStr[0] == '#' || lineStr[0] == '\0')
             continue;
 
-        std::stringstream lineStream(lineStr);
-        lineStream >> temp;
-        auto it = osd_parsers.find(temp);
-        if (it != osd_parsers.end()) {
+        //解析每一行的数据
+        std::stringstream lineStream(lineStr); // 创建一个 std::stringstream 对象 lineStream 来处理当前行的数据
+        lineStream >> temp;                    // 从当前行中提取第一个单词到 temp 中（通常是配置项的关键字）
+        auto it = osd_parsers.find(temp);      // 在 osd_parsers 容器（通常是一个 std::map 或 std::unordered_map）中查找该关键字
+        if (it != osd_parsers.end())
+        { // 如果找到匹配的关键字，则调用与之关联的解析函数 it->second，将 lineStream 和 conf 作为参数传入。这个解析函数将具体解析行中的参数并将其存储在 conf 中。
             it->second(lineStream, conf);
         }
     }
     return 0;
 }
 
+/// @brief 这段代码的主要功能是在检查 para.osdEtc 不为空的情况下，读取对应的文本文件，
+///        将每一行内容存储在 osd_text 中。如果 para.osdDev 为空，则直接存储文本内容；
+///        否则，将 para.osdDev 的值作为第一个元素插入，然后再插入文本内容。
+///        此外，代码使用了互斥锁来确保多线程环境下的线程安全
 void ModuleOsd::handletEtcText()
 {
-    if (!para.osdEtc.empty()) {
-        std::vector<std::string> v;
-        int ret = readFileLines(para.osdEtc.c_str(), &etc_file_mtime, v);
-        if (ret > 0) {
+    // 首先检查 para.osdEtc 是否为空。如果为空，函数立即返回不做任何处理。para.osdEtc 可能是配置文件的路径或文件名
+    if (!para.osdEtc.empty()) 
+    {
+        std::vector<std::string> v; // 定义一个 std::vector<std::string> 类型的变量 v，用于存储从文件中读取的每一行文本
+        int ret = readFileLines(
+                                para.osdEtc.c_str(), 
+                                &etc_file_mtime, 
+                                v);
+        /* 调用 readFileLines 函数读取文件 para.osdEtc 的内容，并将每一行文本存储到 v 中。
+         etc_file_mtime 是一个指向文件修改时间的指针，用来记录文件的最后修改时间。
+         ret 用于存储 readFileLines 函数的返回值，通常表示成功读取的行数 */
+        if (ret > 0) //文件读取结果
+        {
+            // 加锁与更新osd_text
+            // 使用 std::lock_guard<std::mutex> 对象 lk 对 text_mtx 进行加锁，确保在多线程环境下对 osd_text 的访问是线程安全的
             std::lock_guard<std::mutex> lk(text_mtx);
-            if (para.osdDev.empty()) {
+            if (para.osdDev.empty())
+            { // 如果 para.osdDev 为空，则直接将 v 中的内容赋值给 osd_text，即 osd_text = v;
                 osd_text = v;
-            } else {
-                osd_text.clear();
-                osd_text.push_back(para.osdDev);
-                for (auto it : v)
+            }
+            else
+            { // 如果 para.osdDev 不为空
+                osd_text.clear();                // 则先清空 osd_text
+                osd_text.push_back(para.osdDev); // 然后将 para.osdDev 的值作为第一个元素插入到 osd_text 中
+                for (auto it : v)                // ，接着将 v 中的内容逐行插入到 osd_text 中
                     osd_text.push_back(it);
             }
         }
     }
 }
 
+/// @brief 
+/// @return 
 int ModuleOsd::init()
 {
     int ret;
+    // shared_ptr 表示智能指针
     shared_ptr<ModuleCam> cam;
     shared_ptr<ModuleAlsaCapture> capture = nullptr;
     shared_ptr<ModuleMppEnc> enc = nullptr;
@@ -304,27 +340,34 @@ int ModuleOsd::init()
     shared_ptr<ModuleMedia> last_amod = nullptr;
     ImagePara output, input;
 
+    //解析配置文件
     if (parseConfFileInfo(para)) {
         ff_error("Failed to parse Conf file\n");
         return -1;
     }
 
-    handletEtcText();
-    if (!para.osdDev.empty()) {
+    //处理OSD文本信息
+    handletEtcText();               // 调用 handletEtcText 处理OSD（On-Screen Display）文本
+    if (!para.osdDev.empty())
+    { // 如果 osdDev 不为空且 osd_text 为空，则将 osdDev 加入 osd_text。
         std::lock_guard<std::mutex> lk(text_mtx);
         if (osd_text.size() == 0)
+        {
             osd_text.push_back(para.osdDev);
+        }
     }
 
-    if (para.vDev.empty()) {
+    //初始化视频设备，相机
+    if (para.vDev.empty()) 
+    {//若视频设备vDev为空，则返回错误
         printf("video device is empty\n");
         return -1;
     }
-
+    //初始化视频设备模块，
     cam = make_shared<ModuleCam>(para.vDev);
-    cam->setOutputImagePara(para.iPara);  // setOutputImage
-    cam->setProductor(NULL);
-    cam->setBufferCount(12);
+    cam->setOutputImagePara(para.iPara);  // setOutputImage 设置输出图像参数
+    cam->setProductor(NULL);//生产者为NULL
+    cam->setBufferCount(12);//设置缓冲区数量为12
     ret = cam->init();
     if (ret < 0) {
         ff_error("camera init failed\n");
@@ -332,7 +375,9 @@ int ModuleOsd::init()
     }
     last_vmod = cam;
 
-    if (!para.aDev.empty()) {
+    //初始化音频设备
+    if (!para.aDev.empty()) //若音频设备aDev不为空
+    {
         SampleInfo info;
         info.channels = 2;
         info.fmt = SAMPLE_FMT_S16;
@@ -346,8 +391,8 @@ int ModuleOsd::init()
             return -1;
         }
         last_amod = capture;
-        auto aac_enc = make_shared<ModuleAacEnc>(info);
-        aac_enc->setProductor(last_amod);
+        auto aac_enc = make_shared<ModuleAacEnc>(info); // 初始化 AAC 编码器 ModuleAacEnc
+        aac_enc->setProductor(last_amod);               // 并将其设置为音频模块的生产者
         ret = aac_enc->init();
         if (ret < 0) {
             ff_error("aac_enc init failed\n");
@@ -356,11 +401,12 @@ int ModuleOsd::init()
         last_amod = aac_enc;
     }
 
+    //处理视频解码
     output = last_vmod->getOutputImagePara();
     if (output.v4l2Fmt == V4L2_PIX_FMT_MJPEG || output.v4l2Fmt == V4L2_PIX_FMT_H264
         || output.v4l2Fmt == V4L2_PIX_FMT_HEVC) {
-        shared_ptr<ModuleMppDec> dec = make_shared<ModuleMppDec>();
-        dec->setProductor(last_vmod);
+        shared_ptr<ModuleMppDec> dec = make_shared<ModuleMppDec>(); // 初始化视频解码器ModuleMppDec
+        dec->setProductor(last_vmod);//将解码器设置为视频模块的生产者
         dec->setBufferCount(10);
         ret = dec->init();
         if (ret < 0) {
@@ -370,6 +416,7 @@ int ModuleOsd::init()
         last_vmod = dec;
     }
 
+    //图像参数处理与RGA模块初始化
     output = last_vmod->getOutputImagePara();
     {
         output.v4l2Fmt = V4L2_PIX_FMT_BGR24;
@@ -384,7 +431,7 @@ int ModuleOsd::init()
     shared_ptr<ModuleRga> rga = make_shared<ModuleRga>(output, para.oRotate);
     rga->setProductor(last_vmod);
     rga->setBufferCount(8);
-    rga->setOutputDataCallback(this, osdHandlerCallback);
+    rga->setOutputDataCallback(this, osdHandlerCallback); 
     rga->setRgaSchedulerCore(ModuleRga::SCHEDULER_RGA3_DEFAULT);
     ret = rga->init();
     if (ret < 0) {
@@ -392,6 +439,7 @@ int ModuleOsd::init()
         return -1;
     }
 
+    //显示模块初始化
     last_vmod = rga;
     if (para.dDisplayEnable && para.dDisplay >= 0) {
         output = last_vmod->getOutputImagePara();
@@ -429,6 +477,8 @@ int ModuleOsd::init()
             drm_display->setPlaneRect(x, y, w, h);
         }
     }
+
+    //音频播放模块初始化
     if (para.aPlayEnable && last_amod && !para.aPlayDev.empty()) {
         shared_ptr<ModuleAacDec> aac_dec = make_shared<ModuleAacDec>();
         aac_dec->setProductor(last_amod);
@@ -441,7 +491,9 @@ int ModuleOsd::init()
         }
     }
 
-    if (para.pushVideoEnable && para.pPort > 0) {
+    //RTSP推流模块初始化
+    if (para.pushVideoEnable && para.pPort > 0) 
+    {
         output = last_vmod->getOutputImagePara();
         if (para.pPara.width && para.pPara.height) {
             output.width = para.pPara.width;
@@ -461,7 +513,9 @@ int ModuleOsd::init()
         }
 
         enc_r = make_shared<ModuleMppEnc>(ENCODE_TYPE_H264,
-                                          para.pFps, para.pFps << 1, (para.pFps / 30.0) * 2048);
+                                          para.pFps, 
+                                          para.pFps << 1, 
+                                          (para.pFps / 30.0) * 2048);
         enc_r->setProductor(push_rga);
         enc_r->setBufferCount(6);
         enc_r->setDuration(0);
@@ -482,7 +536,9 @@ int ModuleOsd::init()
         }
     }
 
-    if (para.pushAudioEnable && last_amod) {
+    //音频推流模块初始化
+    if (para.pushAudioEnable && last_amod) 
+    {
         auto rtsp_s_a = make_shared<ModuleRtspServerExtend>(rtsp_s, para.pPath.c_str(), para.pPort);
         rtsp_s_a->setProductor(last_amod);
         rtsp_s_a->setAudioParameter(MEDIA_CODEC_AUDIO_AAC);
@@ -494,7 +550,9 @@ int ModuleOsd::init()
         }
     }
 
-    if (!para.oFile.empty()) {
+    //文件写入模块与初始化
+    if (!para.oFile.empty()) 
+    {
         enc = make_shared<ModuleMppEnc>(para.eType, para.eFps,
                                         para.eFps << 1, (para.eFps / 30.0) * 2048);
         enc->setProductor(last_vmod);
@@ -527,6 +585,7 @@ int ModuleOsd::init()
         }
     }
 
+    //模块指针的报错和返回
     v_source = cam;
     a_source = capture;
     f_enc = enc;
@@ -578,26 +637,37 @@ void ModuleOsd::stop()
     }
 }
 
+/// @brief  通过一个循环不断检查 event 的状态，根据不同的事件位进行文件操作和 OSD 文本更新，
+///         同时还负责广播信息的发送
 void ModuleOsd::osdProcess()
 {
-    while (work_flag) {
+    while (work_flag) 
+    {
         {
+            //使用互斥锁 event_mtx 保护共享资源，以防止多个线程同时访问
             std::unique_lock<std::mutex> lk(event_mtx);
-            if (event == 0)
+            if (event == 0)//如果 event 为 0，表示没有事件发生
                 event_conv.wait_for(lk, std::chrono::milliseconds(time_out));
-            if (event & (1 << 1)) {
+            if (event & (1 << 1)) 
+            {
                 continue;
-            } else if (event & 1) {
+            }
+            else if (event & 1) 
+            {
                 event &= ~1;
                 lk.unlock();
-                if (f_enc != nullptr && writer != nullptr) {
-                    f_enc->stop();
+                //文件操作
+                if (f_enc != nullptr && writer != nullptr) 
+                {
+                    f_enc->stop();//停止编码
                     std::string filePath = para.oFileDir + ptsToTimeStr(0, para.oFile) + ".mp4";
-                    createDirectory(filePath.c_str());
-                    writer->changeFileName(filePath);
-                    f_enc->start();
-                    if (para.fFreeSize) {
-                        while (para.fFreeSize > getSystemFreeSize(filePath.c_str())) {
+                    createDirectory(filePath.c_str());  //创建新的目录来保存新的文件
+                    writer->changeFileName(filePath);   //更改文件写入器的文件路径
+                    f_enc->start();//重新启动编码
+                    if (para.fFreeSize)//磁盘空间管理
+                    {                  // 如果设置了 para.fFreeSize，则循环检查当前磁盘空间是否足够，如果不够，则删除最旧的文件以释放空间
+                        while (para.fFreeSize > getSystemFreeSize(filePath.c_str())) 
+                        {
                             if (removeDirectoryOldestFile(para.oFileDir.empty() ? filePath.c_str() : para.oFileDir.c_str()) <= 0)
                                 break;
                         }
@@ -605,11 +675,10 @@ void ModuleOsd::osdProcess()
                 }
             }
         }
-
-        handletEtcText();
-
+        handletEtcText();   //处理OSD文本信息
+        // 如果 para.bPort 被设置且大于 0，并且 rtsp 对象为空或者没有客户端连接到 rtsp，则调用 sendBroadcastMessage 发送广播消息
         if (para.bPort > 0 && (!rtsp || rtsp->ClientsIsEmpty()))
-            sendBroadcastMessage(para.bPort, para.osdDev.c_str(), nullptr);
+            sendBroadcastMessage(para.bPort, para.osdDev.c_str(), nullptr); // 广播消息发送
     }
 }
 
@@ -640,8 +709,12 @@ void ModuleOsd::osdHandlerCallback(void* arg, std::shared_ptr<MediaBuffer> media
         std::lock_guard<std::mutex> lk(osd->text_mtx);
         int etc_count = osd->osd_text.size();
         for (int i = 0; i < etc_count; i++) {
-            putText(mat, osd->osd_text[i], point, osd->para.osdTextPara.fontFace,
-                    osd->para.osdTextPara.fontScale, osd->para.osdTextPara.color);
+            putText(mat, 
+                    osd->osd_text[i], 
+                    point, 
+                    osd->para.osdTextPara.fontFace,
+                    osd->para.osdTextPara.fontScale, 
+                    osd->para.osdTextPara.color);
             point.y += line_h;
         }
     }
